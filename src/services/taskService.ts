aa543071502a2f32c65d645155456997f40600d4
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-07-27 15:03:19 -0500

    chore: Refactor taskService to handle errors and add try-catch blocks

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,11 +1,41 @@
 import api from './api';
 
 export const getTasks = async () => {
-    const response = await api.get('/tasks');
-    return response.data.data;
+    try {
+        const response = await api.get('/tasks');
+        return response.data.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
 };
 
 export const createTask = async (task) => {
-    const response = await api.post('/tasks', task);
-    return response.data.data;
+    try {
+        const response = await api.post('/tasks', task);
+        return response.data.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+};
+
+export const updateTask = async (id, task) => {
+    try {
+        const response = await api.put(`/tasks/${id}`, task);
+        return response.data.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+};
+
+export const deleteTask = async (id) => {
+    try {
+        const response = await api.delete(`/tasks/${id}`);
+        return response.data.data;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
 };
