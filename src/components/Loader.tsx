95c651ccb11d54ec469d11d6f5b51119536e0326
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-08-02 13:29:27 -0500

    Refactor TaskModal component to add loading overlay and improve accessibility

---
+++ src/components/Loader.tsx
@@ -0,0 +1,19 @@
+import { Watch } from 'react-loader-spinner';
+import { createPortal } from 'react-dom';
+import styles from '../styles/Home.module.css';
+
+const Loader = () => {
+    return createPortal(
+        <div className={styles.loaderOverlay}>
+            <Watch
+                height="70"
+                width="70"
+                color="#F5D565"
+                ariaLabel="loading-indicator"
+            />
+        </div>,
+        document.body
+    );
+};
+
+export default Loader;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,20 @@ import { getTasks, updateTask, createTask, deleteTask } from '../services/taskSe
 import TaskModal from '../components/TaskModal';
 import { Task } from '../types/task';
 import Swal from 'sweetalert2';
+import Loader from '../components/Loader';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
     const [tasks, setTasks] = useState<Task[]>([]);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
+            setLoading(true);
             const tasks = await getTasks();
+            setLoading(false);
             setTasks(tasks || []);
         }
 
@@ -38,6 +42,7 @@ export default function Home() {
     const handleSaveTask = async (task: Task) => {
         try {
             if (task._id) {
+                setLoading(true);
                 const updatedTask = await updateTask(task._id, task);
                 if (updatedTask) {
                     const updatedTasks = tasks.map(t => (t._id === task._id ? updatedTask : t));
@@ -54,34 +59,45 @@ export default function Home() {
         } catch (error) {
             Swal.fire('Error', 'An error occurred while saving the task', 'error');
         } finally {
+            setLoading(false);
             handleModalClose();
         }
     };
 
     const handleDeleteTask = async (id: string) => {
-        const deletedTask = await deleteTask(id);
-        if (deletedTask) {
-            const updatedTasks = tasks.filter(t => t._id !== id);
-            setTasks(updatedTasks);
+        setLoading(true);
+        try {
+            const deletedTask = await deleteTask(id);
+            if (deletedTask) {
+                const updatedTasks = tasks.filter(t => t._id !== id);
+                setTasks(updatedTasks);
+            }
+        } catch (error) {
+            Swal.fire('Error', 'An error occurred while deleting the task', 'error');
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>My Task Board</h1>
-            <p className={styles.subtitle}>Tasks to keep organised</p>
-            <TaskList tasks={tasks} onTaskClick={handleTaskClick} />
-            <div className={styles.addTaskCard} onClick={handleAddTaskClick}>
-                <img src="./icons/Add_round_duotone.svg" alt="Add Task Icon" />
-                <span>Add new task</span>
+        <>
+            {loading && <Loader />}
+            <div className={styles.container}>
+                <h1 className={styles.title}>My Task Board</h1>
+                <p className={styles.subtitle}>Tasks to keep organised</p>
+                <TaskList tasks={tasks} onTaskClick={handleTaskClick} />
+                <div className={styles.addTaskCard} onClick={handleAddTaskClick}>
+                    <img src="./icons/Add_round_duotone.svg" alt="Add Task Icon" />
+                    <span>Add new task</span>
+                </div>
+                <TaskModal
+                    isOpen={isModalOpen}
+                    onClose={handleModalClose}
+                    task={selectedTask}
+                    onSave={handleSaveTask}
+                    onDelete={handleDeleteTask}
+                />
             </div>
-            <TaskModal
-                isOpen={isModalOpen}
-                onClose={handleModalClose}
-                task={selectedTask}
-                onSave={handleSaveTask}
-                onDelete={handleDeleteTask}
-            />
-        </div>
+        </>
     );
 }
